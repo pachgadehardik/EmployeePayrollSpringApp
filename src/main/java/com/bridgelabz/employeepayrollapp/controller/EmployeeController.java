ff30152e7fbac3bb394684ba430ac72b34989df5
pachgadehardik <pachgadehardik@gmail.com>
2020-11-26 14:34:46 +0530

    Refactored Controller class with ResponseEntities

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -1,9 +1,8 @@
 package com.bridgelabz.employeepayrollapp.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.service.IEmployee;
+import com.bridgelabz.employeepayrollapp.utility.Response;
 
 @RestController
 @RequestMapping("/employee")
@@ -22,30 +23,35 @@ public class EmployeeController {
 	@Autowired
 	private IEmployee iEmployee;
 	
-	@PostMapping
-	public void addEmployee(@RequestBody Employee employee) {
-		iEmployee.addEmployee(employee);
+	@PostMapping("/add")
+	public ResponseEntity<Response> addEmployee(@RequestBody EmployeeDTO employeeDTO) {
+		Response response = iEmployee.addEmployee(employeeDTO);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 	
 	@GetMapping("/getEmployee")
-	public List<Employee> getEmployee() {
-		return iEmployee.getEmployee();
+	public ResponseEntity<Response> getEmployee() {
+		Response response = iEmployee.getEmployee();
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 	
 	@GetMapping("/getEmployee/{id}")
-	public Employee getEmployeeById(@PathVariable("id") long empId) {
-		return iEmployee.getEmployeeById(empId);
+	public  ResponseEntity<Response> getEmployeeById(@PathVariable("id") long empId) {
+		Response response = iEmployee.getEmployeeById(empId);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 	
-	@PutMapping("/updateEmployee")
-	public void edit(@RequestBody Employee employee) {
-		iEmployee.editEmployee(employee);
+	@PutMapping("/update/{id}")
+	public  ResponseEntity<Response> edit(@PathVariable("id") long id,@RequestBody EmployeeDTO employeeDTO) {
+		Response response = iEmployee.editEmployee(id, employeeDTO);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 	
 	
 	@GetMapping("delete/{empId}")
-    public void deleteEmployee(@PathVariable("empId") long empId) {
-		iEmployee.deleteEmployee(empId);
+    public ResponseEntity<Response> deleteEmployee(@PathVariable("empId") long empId) {
+		Response response = iEmployee.deleteEmployee(empId);
+		return new ResponseEntity<Response>(response, HttpStatus.OK);
 	}
 	
 	
