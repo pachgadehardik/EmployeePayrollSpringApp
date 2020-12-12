36283a4841f1c173a5ee82f57c6fc490bf438acc
pachgadehardik <pachgadehardik@gmail.com>
2020-12-12 18:32:49 +0530

    Added userlogin Exception class

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/excpetions/UserLoginException.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.employeepayrollapp.excpetions;
+
+public class UserLoginException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 507347231520004850L;
+
+	private int statusCode;
+	
+	public UserLoginException(String msg, int statusCode) {
+		super(msg);
+		this.statusCode = statusCode;
+	}
+	
+	
+	
+}
