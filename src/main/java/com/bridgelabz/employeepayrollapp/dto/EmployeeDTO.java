f6ff4b77d8a848139e9ce38d781f98491b3819c1
pachgadehardik <pachgadehardik@gmail.com>
2020-12-12 19:29:40 +0530

    Added pattern validation in employee dto class

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -9,10 +11,13 @@ import lombok.Data;
 @Data
 public class EmployeeDTO {
 	
-	@NotNull
+	@NotNull(message = "employee Name cant be null")
 	@Size(min = 3, message = "Enter more than 3 characters")
+	@Pattern(regexp = "^[A-Z] {1}[a-zA-Z\\s]{2,}$", message = "Employee Name is Invalid")
 	private String empName;
+	
 	@NotNull
+	@Min(value = 10000, message = "Salary should be more than 10000")
 	private Long salary;
 		
 }
